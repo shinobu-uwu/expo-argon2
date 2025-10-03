7fbcc59c21964bf52def2342dfa3758c8c2da0e9
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2025-10-03 16:46:35 -0300

    Add readme and example

--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 
 Argon2 bindings for expo apps
 
+
+## Usage
+
+First install the package:
+```bash
+npm install expo-argon2
+```
+
+Then you can use it in your code like this:
+
+```typescript
+import { hash } from 'expo-argon2';
+
+const hashedPassword = await hash('mypassword', 'random_salt', {
+  timeCost: 3,
+  memoryCost: 4096,
+  parallelism: 1,
+  hashLength: 32,
+  type: 'argon2id',
+});
+console.log(hashedPassword);
+```
+
+For more details, see the example project in the `example` folder.
+
--- example/App.tsx
+++ example/App.tsx
@@ -1,118 +1,57 @@
 import { hash } from 'expo-argon2';
 import * as Clipboard from 'expo-clipboard';
 import { useState } from 'react';
-import { Button, SafeAreaView, Text, TextInput, View } from 'react-native';
+import { View } from 'react-native';
+import { Button, PaperProvider, Text, TextInput } from 'react-native-paper';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   const [password, setPassword] = useState('');
   const [salt, setSalt] = useState('');
-  const [hashValue, setHashValue] = useState<{
-    encoded: string;
-    hex: string;
-    raw: Uint8Array;
-  }>({ encoded: '', hex: '', raw: new Uint8Array() });
+  const [result, setResult] = useState('');
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View
+    <PaperProvider>
+      <SafeAreaView
         style={{
           flex: 1,
-          gap: 8,
           justifyContent: 'center',
-          alignItems: 'center',
+          padding: 16,
+          alignContent: 'space-evenly',
         }}
       >
-        <TextInput
-          style={{ width: '100%' }}
-          value={password}
-          onChangeText={setPassword}
-          placeholder="Password"
-        />
-        <TextInput
-          style={{ width: '100%' }}
-          value={salt}
-          onChangeText={setSalt}
-          placeholder="Salt"
-        />
-        <View style={{ flexDirection: 'row', gap: 10, margin: 20 }}>
-          <Text>Encoded: </Text>
-          <Text>{hashValue.encoded}</Text>
+        <View style={{ gap: 8 }}>
+          <TextInput
+            mode="outlined"
+            label="Password"
+            secureTextEntry
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TextInput
+            mode="outlined"
+            label="Salt"
+            value={salt}
+            onChangeText={setSalt}
+          />
+          <Button
+            mode="contained"
+            onPress={async () => {
+              const hashed = hash(password, salt);
+              setResult(hashed.encoded);
+            }}
+          >
+            Hash
+          </Button>
+          <Button
+            mode="contained"
+            onPress={async () => Clipboard.setStringAsync(result)}
+          >
+            Copy hash
+          </Button>
+          <Text variant="bodyLarge">{result ?? ''}</Text>
         </View>
-        <View style={{ flexDirection: 'row', gap: 10, margin: 20 }}>
-          <Text>Hex: </Text>
-          <Text>{hashValue.hex}</Text>
-        </View>
-        <View style={{ flexDirection: 'row', gap: 10, margin: 20 }}>
-          <Text>Raw: </Text>
-          <Text>{hashValue.raw}</Text>
-        </View>
-        <Button
-          onPress={() => {
-            const result = hash(password, salt, {
-              type: 'argon2d',
-              timeCost: 3,
-              memoryCost: 65536,
-              parallelism: 4,
-              hashLength: 32,
-            });
-            setHashValue(result);
-          }}
-          title="Hash"
-        />
-        <Button
-          onPress={() => {
-            const encoder = new TextEncoder();
-            const array = encoder.encode(password);
-            const result = hash(array, salt, {
-              type: 'argon2d',
-              timeCost: 3,
-              memoryCost: 65536,
-              parallelism: 4,
-              hashLength: 32,
-            });
-            setHashValue(result);
-          }}
-          title="Hash (byte array)"
-        />
-        <Button
-          onPress={() => Clipboard.setStringAsync(hashValue.encoded)}
-          title="Copy"
-        />
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
-
-function Group(props: { name: string; children: React.ReactNode }) {
-  return (
-    <View style={styles.group}>
-      <Text style={styles.groupHeader}>{props.name}</Text>
-      {props.children}
-    </View>
-  );
-}
-
-const styles = {
-  header: {
-    fontSize: 30,
-    margin: 20,
-  },
-  groupHeader: {
-    fontSize: 20,
-    marginBottom: 20,
-  },
-  group: {
-    margin: 20,
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#eee',
-  },
-  view: {
-    flex: 1,
-    height: 200,
-  },
-};
