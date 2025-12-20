6a2ad81ea27b1fbcea31180e85f734b248ff8a8c
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-12-20 12:22:11 +0100

    Add `hashAsync`, minor improvements
    - Added non-blocking version of this function that hashes in a
    background (expo-modules queue) thread
    - Extracted and fixed some TypeScript types for options, improved TSDoc.
    - Fixed example - added default options={} to fix Expo type cast errors
    - native module expects options to be non-null object
    - Added salt length check - explicit clean error message for too short
    salts instead of "Operation couldn't be completed"

--- src/ExpoArgon2Module.ts
+++ src/ExpoArgon2Module.ts
@@ -1,5 +1,43 @@
 import { NativeModule, requireNativeModule } from 'expo';
 
+export interface Argon2Options {
+  /**
+   * Defaults to 3
+   */
+  timeCost?: number;
+  /**
+   * Defaults to 4096
+   */
+  memoryCost?: number;
+  /**
+   * Defaults to 1
+   */
+  parallelism?: number;
+  /**
+   * Defaults to 32
+   */
+  hashLength?: number;
+  /**
+   * Defaults to 'argon2id'
+   */
+  type?: 'argon2d' | 'argon2i' | 'argon2id';
+}
+
+export interface Argon2Result {
+  /**
+   * Raw hash bytes
+   */
+  raw: Uint8Array;
+  /**
+   * Argon2-style encoded hash
+   */
+  encoded: string;
+  /**
+   * Hash bytes, hex-encoded
+   */
+  hex: string;
+}
+
 declare class ExpoArgon2Module extends NativeModule {
   /**
    * Hashes a password using the Argon2 algorithm.
@@ -11,18 +49,21 @@ declare class ExpoArgon2Module extends NativeModule {
   hash(
     password: Uint8Array,
     salt: Uint8Array,
-    options?: {
-      timeCost?: number; // default 3
-      memoryCost?: number; // default 4096
-      parallelism?: number; // default 1
-      hashLength?: number; // default 32
-      type?: 'argon2d' | 'argon2i' | 'argon2id'; // default 'argon2id'
-    },
-  ): {
-    raw: Uint8Array;
-    encoded: string;
-    hex: string;
-  };
+    options?: Argon2Options,
+  ): Argon2Result;
+
+  /**
+   * Asynchronously hashes a password using the Argon2 algorithm.
+   * @param password The password to hash.
+   * @param salt A unique salt for the password.
+   * @param options parameters for the hashing process. Defaults to timeCost=3, memoryCost=4096, parallelism=1, hashLength=32, type='argon2id'.
+   * @returns A promise resolving to an object containing the raw hash, encoded hash, and hex representation of the hash.
+   */
+  hashAsync(
+    password: Uint8Array,
+    salt: Uint8Array,
+    options?: Argon2Options,
+  ): Promise<Argon2Result>;
 }
 
 // This call loads the native module object from the JSI.
